8f690add70cde8f59f3455d685417daf187a506c
Sven Duve <svenduve@gmail.com>
2022-05-27 11:22:11 +0200

    example 2

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 
+// stateless fucntiona component
+// always return JSX I mean alwas return somehtiung
+
+
 function Greeting() {
-  return <h1>This is me and my first component</h1>;
+  return (
+  <div>
+    <h1>Hellod This is some react frameworkd stuffy</h1>
+    <ul>
+      <li>super boring</li>
+    </ul>
+  </div>
+  );
 }
+
+// const Greeting = () =>{
+//   return React.createElement('h1',{},'hello I am a cockmuncher');
+// }
 
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
---
+++ tutorial/src/testing.js
@@ -0,0 +1,13 @@
+// type rafce and get a component temolate, coms from ES7 React thingi
+
+
+import React from 'react'
+
+const NewComonent = () => {
+  return (
+    <div>NewComonent</div>
+  )
+}
+
+export default NewComonent
+
